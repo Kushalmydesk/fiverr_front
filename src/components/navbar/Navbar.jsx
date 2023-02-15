58d309f075f5fcb8720b6007dfe59358ad95aef7
kushal_mydesk <kushal99mondal@gmail.com>
2023-02-15 15:08:47 +0530

    Added User & Menu Functionalities

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import "./Navbar.scss";
 const Navbar = () => {
 
     const [active, setActive] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const isActive = () => {
         window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -16,7 +17,13 @@ const Navbar = () => {
         return () => {
             window.removeEventListener("scroll", isActive);
         };
-    }, [])
+    }, []);
+
+    const currentUser = {
+        id: 1,
+        username: "John Doe",
+        isSeller: true
+    }
 
     return (
         <div className={active ? 'navbar active' : 'navbar'}>
@@ -32,8 +39,28 @@ const Navbar = () => {
                     <span>Explore</span>
                     <span>English</span>
                     <span>Sign In</span>
-                    <span>Become a Seller</span>
-                    <button>Join</button>
+                    {!currentUser?.isSeller && <span>Become a Seller</span>}
+                    {!currentUser && <button>Join</button>}
+                    {currentUser &&
+                        <div className="user" onClick={() => setOpen(!open)}>
+                            <img src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png" alt="" />
+                            <span>{currentUser?.username}</span>
+                            {open && <div className="options">
+                                {
+                                    currentUser?.isSeller && (
+                                        <>
+                                            <span>Gigs</span>
+                                            <span>Add New Gig</span>
+
+                                        </>
+                                    )
+                                }
+                                <span>Orders</span>
+                                <span>Messages</span>
+                                <span>Log Out</span>
+                            </div>}
+                        </div>
+                    }
                 </div>
             </div>
             {active &&
